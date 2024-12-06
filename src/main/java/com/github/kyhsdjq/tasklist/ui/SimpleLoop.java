cc9b1142f4172a31c7ef8ef4b5960056d6a7675c
ZiyanYang <yang_ziyan@outlook.com>
2024-12-06 15:17:06 +0800

    Inject resource leak
    Signed-off-by: ZiyanYang <yang_ziyan@outlook.com>

--- src/main/java/com/github/kyhsdjq/tasklist/ui/SimpleLoop.java
+++ src/main/java/com/github/kyhsdjq/tasklist/ui/SimpleLoop.java
@@ -5,6 +5,9 @@ import com.github.kyhsdjq.tasklist.ui.window.FilterWindow;
 import com.github.kyhsdjq.tasklist.ui.window.TaskWindow;
 import com.github.kyhsdjq.tasklist.ui.window.TimelineWindow;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 public class SimpleLoop {
@@ -40,6 +43,20 @@ public class SimpleLoop {
     }
 
     public static void main(String[] args) {
+        // injecting resource leak
+        String filePath = "D:/reminder.ps1";
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(filePath));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
         SimpleLoop simpleTaskList = new SimpleLoop();
         simpleTaskList.startLoop();
     }
